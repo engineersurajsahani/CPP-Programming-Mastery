152eb770e2aa5f456554663ddaa1ea73a3c77bf2
Suraj Sahani <93707688+engineersurajsahani@users.noreply.github.com>
2024-12-22 12:26:50 +0530

    Create basic_concept.cpp

---
+++ basic_concept.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a=5;
+    float f=5.5;
+    char c='h';
+    string name="Suraj Sahani";
+    
+    cout<<a<<endl;
+    cout<<f<<endl;
+    cout<<c<<endl;
+    cout<<name<<endl;
+    
+}
